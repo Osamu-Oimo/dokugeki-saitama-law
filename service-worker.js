03401ca06448576948d9248804f18aaf59f01fc6
Osamu-Oimo <rasukul@hotmail.com>
2026-09-13 20:17:12 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C="dokugeki-law-v3-1";
+const C="dokugeki-law-v4";
 const A=["./","./index.html","./style.css","./app.js","./questions.json","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
